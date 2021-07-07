28a013adb1add75c904098dbafc29b1bdefeb351
Your Name <you@example.com>
2021-07-07 14:18:01 +0300

    Добавлены миграции под состояния ПП

--- web/scripts/Migration.sql
+++ web/scripts/Migration.sql
@@ -1,2 +1,9 @@
-ALTER TABLE `missions`.`missionitems` 
+ALTER TABLE `missions`.`missionitems`
 CHANGE COLUMN `description` `report` VARCHAR(1000) NULL DEFAULT NULL ;
+
+'07/08/2021
+DROP TABLE `missions`.`mission_fact`;
+
+ALTER TABLE `missions`.`missionitems`
+ADD COLUMN `status` INT NOT NULL DEFAULT 1011 AFTER `missionuid`;
+  
