4c2e027f4abf8a358ecf2bcaffbe62cb957ff913
OlegBPavlov <pavlovob@yandex.ru>
2021-07-09 21:30:25 +0300

    -

--- web/index.php
+++ web/index.php
@@ -6,18 +6,18 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 
 // -----------------  Константы ------------------
 //статус поручений
-define ('STATE_OPEN', 1001); //поручения открыты для редактирования
-define ('STATE_CLOSE', 1002); //закрыты
+define ('STATE_OPEN', 1001);    //поручения открыты для редактирования
+define ('STATE_CLOSE', 1002);   //закрыты
 define ('STATE_DELETED', 1003); //удалены
 //статус пунктов поручений
-define ('STATE_INWORK', 1011); //Назначены, в работе. по умолчанию устанавливается в БД при создании
-define ('STATE_DONE', 1012); //Выполнены (устанавливается куратором)
+define ('STATE_INWORK', 1011);  //Назначены, в работе. по умолчанию устанавливается в БД при создании
+define ('STATE_DONE', 1012);    //Выполнены (устанавливается куратором)
 //роли пользователей
-define ('USERTYPE_NONE', 0);    //Пользователь по умолчанию
-define ('USERTYPE_ADMIN', 1);   //Администратор
-define ('USERTYPE_ASSIGNER', 2);//Кураторы
-define ('USERTYPE_EXECUTER', 3);//Исполнителиcd
-define ('USERTYPE_BLOCKED', 4);    //Пользователь заблокирован
+define ('USERTYPE_NONE', 0);      //Пользователь по умолчанию
+define ('USERTYPE_ADMIN', 1);     //Администратор
+define ('USERTYPE_ASSIGNER', 2);  //Кураторы
+define ('USERTYPE_EXECUTER', 3);  //Исполнителиcd
+define ('USERTYPE_BLOCKED', 4);   //Пользователь заблокирован
 
 
 //------------------ конец определений
