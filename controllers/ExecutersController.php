f5e0d0f442be15b91c143795b76f77137f130e0b
OlegBPavlov <pavlovob@gmail.com>
2021-07-12 19:51:04 +0300

    Exefuters bugfix

--- controllers/ExecutersController.php
+++ controllers/ExecutersController.php
@@ -66,9 +66,8 @@ class ExecutersController extends Controller{
   //редактирование записи исполнителя
   public function actionUpdate($id)    {
     $model = $this->findModel($id);
-    if ($model->load(Yii::$app->request->post()))  {
-      $model->changed = date('Y-m-d G:i:s', time());
-      $model->save();
+    $model->changed = date('Y-m-d G:i:s', time());
+    if ($model->load(Yii::$app->request->post()) && $model->save())  {
       History::log('Отредактирован испонитель в справочнике',implode(', ',$model->toArray()));
       return $this->redirect(['view', 'id' => $model->uid]);
     }
