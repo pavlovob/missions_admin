d17b91f0874602285ece788b82c702677c80ae9e
Your Name <you@example.com>
2021-07-26 16:58:04 +0300

    samples

--- models/Missions.php
+++ models/Missions.php
@@ -90,6 +90,24 @@ class Missions extends \yii\db\ActiveRecord {
 
         $writer = new Xlsx($spreadsheet);
         $writer->save('hello world.xlsx');
+
+        $file = 'hello world.xlsx';
+
+        if (file_exists($file)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename='.basename($file));
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($file));
+            ob_clean();
+            flush();
+            readfile($file);
+            // exit;
+        }
+
         History::log('Поручения с кодом '.$id.' выгружены в MS Excel');
       }else{
         throw new NotFoundHttpException('Не найдена запись поручений');
