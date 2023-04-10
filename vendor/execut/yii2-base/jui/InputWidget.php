ee6c2720bfc792f5288ce34438c4b25aaeabeb77
Your Name <you@example.com>
2023-04-10 16:20:53 +0300

    fixes

--- vendor/execut/yii2-base/jui/InputWidget.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace execut\yii\jui;
-use execut\yii\jui\widget\Renderer;
-use yii\helpers\Html;
-use yii\helpers\Inflector;
-
-class InputWidget extends \yii\jui\InputWidget {
-    use WidgetTrait;
-} 
--- vendor/execut/yii2-base/jui/Widget.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace execut\yii\jui;
-use execut\yii\jui\widget\Renderer;
-use yii\helpers\Html;
-use yii\helpers\Inflector;
-
-class Widget extends \yii\jui\Widget {
-    use WidgetTrait;
-} 
